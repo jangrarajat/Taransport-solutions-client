0b73704f991a3cf1956dda9260ab67f14f3c4dd2
Rajat jangra <rajatjangra940@gmail.com>
2026-03-13 18:52:22 +0530

    button loader theme ishu

--- src/components/loaders/ButtonLoaders.jsx
+++ src/components/loaders/ButtonLoaders.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ButtonLoaders() {
     return (
-        <div className="h-5 w-5 rounded-full border-2 border-t-transparent border-b-white animate-spin"></div>
+        <div className="h-5 w-5 rounded-full border-2 border-t-transparent border-b-gray-900 dark:border-b-white animate-spin"></div>
     );
 }
 
